980c078af74158e26c0816c7334526b26b9a3a92
Jerome.Ortega <jerome.ortega@mosaic.com>
2018-08-14 17:51:46 -0400

    hello

--- azurefunctionscicd/HttpTrigger.cs
+++ azurefunctionscicd/HttpTrigger.cs
@@ -23,7 +23,7 @@ namespace azurefunctionscicd
             name = name ?? data?.name;
 
             return name != null
-                ? (ActionResult)new OkObjectResult($"Hello there, {name}")
+                ? (ActionResult)new OkObjectResult($"Hello there, {name}. It's nice to meet you.")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
         }
     }
